eff562ea9007db08b24abc01e91a78e163d8cb35
agouy <contact@agouy.com>
2021-02-14 21:36:48 +0000

    first version

--- main.R
+++ main.R
@@ -1,9 +1,18 @@
 library(tercen)
 library(dplyr)
+library(mclust)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
+clust.df <- function(df) {
+  X <- df[, c(".x", ".y")]
+  do.bic <- mclustBIC(X)
+  model <- Mclust(X, x = do.bic)
+  out <- data.frame(df, cluster = model$classification)
+  return(out)
+}
+
+(ctx = tercenCtx()) %>% 
+  select(.x, .y, .ci, .ri) %>%
   group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  do(clust.df(.)) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,21 @@
 library(tercen)
 library(dplyr)
+library(mclust)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "2553cb89b6ec3bc593e238e0df047713")
+options("tercen.stepId"     = "7f237d16-924d-4913-8ea5-ab44b3bed7fb")
 
-getOption("tercen.workflowId")
-getOption("tercen.stepId")
+clust.df <- function(df) {
+  X <- df[, c(".x", ".y")]
+  do.bic <- mclustBIC(X)
+  model <- Mclust(X, x = do.bic)
+  out <- data.frame(df, cluster = model$classification)
+  return(out)
+}
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
+(ctx = tercenCtx()) %>% 
+  select(.x, .y, .ci, .ri) %>%
   group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+  do(clust.df(.)) %>%
   ctx$addNamespace() %>%
   ctx$save()
