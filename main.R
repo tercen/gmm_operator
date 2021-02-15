fa5ffcbd2ea2b84fa7aa727f61bb25288be1b89e
agouy <contact@agouy.com>
2021-02-15 21:54:37 +0000

    new input format

--- main.R
+++ main.R
@@ -1,18 +1,16 @@
 library(tercen)
 library(dplyr)
 library(mclust)
+library(reshape2)
 
-clust.df <- function(df) {
-  X <- df[, c(".x", ".y")]
-  do.bic <- mclustBIC(X)
-  model <- Mclust(X, x = do.bic)
-  out <- data.frame(df, cluster = model$classification)
-  return(out)
-}
+data = (ctx = tercenCtx())  %>% 
+  select(.ci, .ri, .y) %>% 
+  reshape2::acast(.ci ~ .ri, value.var='.y', fill=NaN, fun.aggregate=mean) 
 
-(ctx = tercenCtx()) %>% 
-  select(.x, .y, .ci, .ri) %>%
-  group_by(.ci, .ri) %>%
-  do(clust.df(.)) %>%
+do.bic <- mclustBIC(data)
+model <- Mclust(data, x = do.bic)
+
+data.frame(.ci = seq(from = 0, to = length(model$classification) - 1),
+           cluster=paste0("cluster", model$classification)) %>%
   ctx$addNamespace() %>%
   ctx$save()
